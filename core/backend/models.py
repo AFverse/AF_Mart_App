0617606b625b9b767771e652827a5c41616aa561
Mudassar Amin <mudasiramin456@gmail.com>
2023-11-12 18:15:24 +0500

    completed database models
    Maybe this may take some more modifications

--- core/backend/models.py
+++ core/backend/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+import uuid
 
 class User(models.Model):
     email = models.CharField(max_length=200)
@@ -25,8 +26,7 @@ class Token(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     
     def __str__(self) -> str:
-        return self.user.phone
-    
+        return self.user.phone 
     
 class PassResetToken(models.Model):
     token = models.CharField(max_length=5000) 
@@ -36,4 +36,108 @@ class PassResetToken(models.Model):
     
     def __str__(self) -> str:
         return self.user.phone
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    disc = models.TextField()
+    parent_category = models.ForeignKey('self', null=True, blank=True, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Reviews(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='reviews')
+    product = models.ForeignKey('Product', on_delete=models.CASCADE, related_name='reviews')
+    rating = models.FloatField()
+    comment = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('user', 'product')
+
+    
+    
+class Discount(models.Model):
+    name = models.CharField(max_length=100)
+    disc = models.TextField()
+    percentage = models.FloatField()
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Brand(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=100)
+    disc = models.TextField()
+    slug = models.SlugField()
+    SKU = models.CharField(max_length=50)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    picture = models.ImageField(upload_to='product_images')
+    discount = models.ForeignKey(Discount, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
+    inventory = models.PositiveIntegerField()
+    brand = models.ForeignKey(Brand, on_delete=models.CASCADE, related_name='products')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class ProductVariation(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    inventory = models.PositiveIntegerField()
+
+    def __str__(self):
+        return f"{self.product.title} - {self.variation.name}"
+
+class Cart_itmes(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+    
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class OrderItems(models.Model):
+    order = models.ForeignKey('Order', on_delete=models.CASCADE, related_name='order_items')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Order(models.Model):
+    
+    STATUS_OPTIONS = [
+        ('cart', 'CART'),
+        ('pending', 'PENDING'),
+        ('completed', 'COMPLETED')
+    ]
+    order_id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    status = models.CharField(max_length=20, choices=STATUS_OPTIONS, default='cart')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    
+class UserItem(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+    is_in_cart = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
 
