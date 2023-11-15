9124436b55a0aff21238fca7c44d7e40d6acae91
Mudassar Amin <mudasir45@users.noreply.github.com>
2023-11-15 10:22:38 +0500

    models are final migrations done

--- core/backend/models.py
+++ core/backend/models.py
@@ -38,10 +38,17 @@ class PassResetToken(models.Model):
         return self.user.phone
 
 
+class ParentCategory(models.Model):
+    name = models.CharField(max_length=100)
+    disc = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    
 class Category(models.Model):
     name = models.CharField(max_length=100)
     disc = models.TextField()
-    parent_category = models.ForeignKey('self', null=True, blank=True, on_delete=models.CASCADE)
+    parent_category = models.ForeignKey(ParentCategory, null=True, blank=True, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
@@ -70,7 +77,8 @@ class Discount(models.Model):
 class Brand(models.Model):
     name = models.CharField(max_length=100, unique=True)
     description = models.TextField()
-
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
     def __str__(self):
         return self.name
 
@@ -95,6 +103,8 @@ class ProductVariation(models.Model):
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     inventory = models.PositiveIntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f"{self.product.title} - {self.variation.name}"
