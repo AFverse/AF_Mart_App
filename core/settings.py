29e185bb01a496b9559d2301de8b09e8232a6c92
Aun Ali <93984843+aaun18110@users.noreply.github.com>
2023-12-22 11:35:30 +0500

    commit

--- core/settings.py
+++ core/settings.py
@@ -165,6 +165,8 @@ EMAIL_HOST_PASSWORD = 'lnsgbhtprmuhwcza'
 CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:8000",
     "http://localhost:3000",
+    "http://192.168.0.112:8000",
+
     
     ]
 # password reset email expire in seconds
